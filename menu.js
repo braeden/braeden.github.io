3dc998eac8d42bfdcb2107ca080bc0747e9889c9
Braeden <braedensmith2@gmail.com>
2015-11-07 16:20:54 -0500

    Removed default schedule, revised whole website

--- menu.js
+++ menu.js
@@ -1,11 +1,57 @@
-//http://codepen.io/nodws/pen/Cbjhs
+/**
+ * hamburger.js
+ *
+ * Mobile Menu Hamburger
+ * =====================
+ * A hamburger menu for mobile websites
+ *
+ * Created by Thomas Zinnbauer YMC AG  |  http://www.ymc.ch
+ * Date: 21.05.13
+ */
 
-// @nodws
-(function() {
-  $(".icon").on('click', function() {
-    $(this).toggleClass('active');
-    $('.box').toggleClass('active');
-    return $('.tap').addClass('active');
-  });
+jQuery(document).ready(function () {
 
-}).call(this);
+    function menuIn() {
+	    jQuery('#content').css('min-height', jQuery(window).height());
+
+        jQuery('nav').css('opacity', 1);
+
+        //set the width of primary content container -> content should not scale while animating
+        var contentWidth = jQuery('#content').width();
+
+        //set the content with the width that it has originally
+        jQuery('#content').css('width', contentWidth);
+
+        //display a layer to disable clicking and scrolling on the content while menu is shown
+        jQuery('#contentLayer').css('display', 'block');
+
+        //disable all scrolling on mobile devices while menu is shown
+        jQuery('#container').bind('touchmove', function (e) {
+            e.preventDefault()
+        });
+
+        //set margin for the whole container with a jquery UI animation
+        jQuery("#container").animate({"marginLeft": ["25vh", 'easeOutExpo']}, {
+            duration: 700
+        });
+	    $(this).one("click", menuOut);
+	}
+	function menuOut() {
+	           //enable all scrolling on mobile devices when menu is closed
+        jQuery('#container').unbind('touchmove');
+
+        //set margin for the whole container back to original state with a jquery UI animation
+        jQuery("#container").animate({"marginLeft": ["-1", 'easeOutExpo']}, {
+            duration: 700,
+            complete: function () {
+                jQuery('#content').css('width', 'auto');
+                jQuery('#contentLayer').css('display', 'none');
+                jQuery('nav').css('opacity', 0);
+                jQuery('#content').css('min-height', 'auto');
+
+            }
+        });
+	    $(this).one("click", menuIn);
+	}
+	$("#hamburger").one("click", menuIn);
+});
