ca81803e1077a45842261640112db15fc209e15a
Braeden <braedensmith2@gmail.com>
2015-12-12 22:26:08 -0500

    Update screenshots and readme

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## About Page appearace:
 
-![Desktop Screenshot](/img/desktop-screenshot.png?raw=true "Desktop Screenshot")
+![Desktop Screenshot](/img/desktop01.png?raw=true "Desktop Screenshot")
 
-![Mobile Screenshot](/img/mobile-screenshot.png?raw=true "Mobile Screenshot")
+![Mobile Screenshot](/img/mobile01.png?raw=true "Mobile Screenshot")
+
+![Desktop Screenshot](/img/desktop02.png?raw=true "Desktop Screenshot")
+
+![Mobile Screenshot](/img/mobile02.png?raw=true "Mobile Screenshot")
+
+![Mobile Screenshot](/img/mobile03.png?raw=true "Mobile Screenshot")
--- js/nightMode.js
+++ js/nightMode.js
@@ -37,18 +37,5 @@ function nightMode(cb) {
  	document.getElementById("main-content").className.replace
     ( /(?:^|\s)mdl-color-text--grey-300(?!\S)/g , '' )
     document.getElementById("main-content").className += " mdl-color-text--grey-800";
-
-/* For future use to keep the ribbon from getting cut
-    var parent = document.getElementById("insert-ribbons");
-	var child = document.getElementById("ribbon");
-	parent.removeChild(child);
-	var newRibbon = document.createElement( 'div' )
-	var placeToInsert = document.getElementById("insert-ribbons");
-  	placeToInsert.appendChild(newRibbon);
-  	newRibbon.className = "ribbon";
-  	newRibbon.id = "ribbon"
-
-*/
-
   }
 }
