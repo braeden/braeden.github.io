54e7b552db2200bc6fd68c758b9b7d590900e209
Braeden Smith <braedensmith2@gmail.com>
2016-01-21 09:58:30 -0500

    Update fallDown.js

--- js/fallDown.js
+++ js/fallDown.js
@@ -7,3 +7,14 @@ function checkAccel() {
     window.addEventListener("devicemotion", accelerometerUpdate, true);
   }
 }
+function accelerometerUpdate(e) {
+   var aX = event.accelerationIncludingGravity.x*1;
+   var aY = event.accelerationIncludingGravity.y*1;
+   var aZ = event.accelerationIncludingGravity.z*1;
+   //The following two lines are just to calculate a
+   // tilt. Not really needed. 
+   xPos = Math.atan2(aY, aZ);
+   yPos = Math.atan2(aX, aZ);
+   console.log(xPos)
+   console.log(yPos)
+}
