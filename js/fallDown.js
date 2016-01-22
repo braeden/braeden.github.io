15bf651871762ec9010b174b157ce5dc03faae48
Braeden Smith <braedensmith2@gmail.com>
2016-01-22 08:08:02 -0500

    Update fallDown.js

--- js/fallDown.js
+++ js/fallDown.js
@@ -1,47 +1,14 @@
-var x = 0, y = 0,
-    vx = 0, vy = 0,
-	ax = 0, ay = 0;
-	
-var sphere = document.getElementById("sphere");
-
-if (window.DeviceMotionEvent != undefined) {
-	window.ondevicemotion = function(e) {
-		ax = event.accelerationIncludingGravity.x * 5;
-		ay = event.accelerationIncludingGravity.y * 5;
-
-		if ( e.rotationRate ) {
-		}		
-	}
-
-	setInterval( function() {
-		var landscapeOrientation = window.innerWidth/window.innerHeight > 1;
-		//if ( landscapeOrientation) {
-		//	vx = vx - ay;
-		//	vy = vy + ax;
-		//} else {
-			vx -= ay;
-			vy += ax;			
-		//}
-		vx = vx * 0.98;
-		vy = vy * 0.98;
-		y = parseInt(y + vy / 50);
-		x = parseInt(x + vx / 50);
-		
-		boundingBoxCheck();
-		
-		sphere.style.top = x + "px";
-		sphere.style.left = y + "px";
-		
-	}, 25);
-} else {
-	console.log("No Accelerometer")
-} 
+var c = document.getElementById("game_canvas");
+var ctx = c.getContext("2d");
 
 
-function boundingBoxCheck(){
-	if (x<0) { x = 0; vx = -vx; }
-	if (y<0) { y = 0; vy = -vy; }
-	if (x>document.documentElement.clientWidth-20) { x = document.documentElement.clientWidth-20; vx = -vx; }
-	if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
-	
+function draw() {
+	ctx.moveTo(0,0);
+	ctx.lineTo(200,100);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(95,50,40,0,2*Math.PI);
+	ctx.stroke();
+	requestAnimationFrame(draw);
 }
+draw();
