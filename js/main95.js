c1e44f90b82e6e2e3c7e0e9b2a9f323f64d539da
Braeden Smith <braedensmith2@gmail.com>
2021-03-11 12:53:04 -0600

    initial testing for w95 UI revamp

--- js/main95.js
+++ js/main95.js
@@ -1,21 +1,3 @@
-const strings = ['Windows Tour']
-const ratio = 9 / 1.5
-const height = 30
-const l = [...document.getElementsByTagName('canvas')]
-l.forEach((elem, i) => {
-    const ctx = elem.getContext('2d');
-    ctx.imageSmoothingEnabled = false
-    elem.width = height * ratio
-    elem.height = height;
-    ctx.font = '20px Source Sans Pro';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle'
-    ctx.fillText(strings[i] || 'Default', elem.width / 2 + .1, elem.height / 2)
-})
-
-
-
 const buttons = [...document.getElementsByClassName('button')]
 buttons.forEach(elem => {
     elem.addEventListener('mousedown', () => elem.classList.add('inset'))
@@ -25,7 +7,7 @@ buttons.forEach(elem => {
 
 let [dx, dy] = [0, 0];
 let moveEnabled = false;
-const dpi = window.devicePixelRatio || 1;
+const dpi = 1 || window.devicePixelRatio;
 ['mousedown', 'touchstart'].forEach(event => {
     document.getElementById('menubar').addEventListener(event, (e) => {
         e.preventDefault();
@@ -39,10 +21,9 @@ const dpi = window.devicePixelRatio || 1;
             tempY = e.clientY;
         }
         const mainWindow = document.getElementById('main-window')
-        dx = tempX * dpi - parseInt(getComputedStyle(mainWindow).left || 0)
-        dy = tempY * dpi - parseInt(getComputedStyle(mainWindow).top || 0)
+        dx = tempX * dpi - mainWindow.getBoundingClientRect().left || 0
+        dy = tempY * dpi - mainWindow.getBoundingClientRect().top || 0
         moveEnabled = true;
-        mainWindow.style.margin = "0";
         mainWindow.style.cursor = "grabbing"
     })
 });
@@ -62,9 +43,10 @@ const dpi = window.devicePixelRatio || 1;
             tempY = e.clientY;
         }
         const mainWindow = document.getElementById('main-window')
+        mainWindow.style.margin = "0";
         mainWindow.style.left = `${tempX * dpi - dx}px`
         mainWindow.style.top = `${tempY * dpi - dy}px`
-    });
+    }, { passive: false });
 });
 
 ['mouseup', 'touchstop'].forEach(event => {
@@ -73,5 +55,5 @@ const dpi = window.devicePixelRatio || 1;
         const mainWindow = document.getElementById('main-window')
         mainWindow.style.cursor = ""
 
-    });
+    }, { passive: false });
 });
