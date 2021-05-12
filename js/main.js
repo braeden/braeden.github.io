7e99654fc1061bad450219c4049b9a9e2e001597
Braeden Smith <braedensmith2@gmail.com>
2021-05-12 13:54:26 -0500

    revert old features

--- js/main.js
+++ js/main.js
@@ -19,3 +19,103 @@ document.addEventListener('touchmove', (e) => {
 // document.addEventListener('mouseenter', () => {
 //     document.getElementById('glow').style.opacity = 1;
 // })
+
+const emojis = ["🙋🏼‍♂️", "👋", "🤚", "🚀", "🤘", "😁", "👨🏼‍💻", "👌", "✌️", "🙌", "🤙", "🖖", "✨"];
+document.getElementById('emoji').innerText = emojis[Math.floor(Math.random() * emojis.length)];
+
+// Easter Egg :)
+document.getElementById('emoji').addEventListener('copy', () => {
+    const unit = 200;
+    const pattern = '-... .-. .- . -.. . -.'.split('').reduce((a, e) => {
+        switch (e) {
+            case '.':
+                a.push(unit, unit);
+                break;
+            case '-':
+                a.push(3 * unit, unit);
+                break;
+            case ' ':
+                a[a.length - 1] += 3 * unit
+                break;
+            case '|':
+                a[a.length - 1] += 7 * unit
+                break;
+        }
+        return a
+    }, []);
+    console.log(window.navigator.vibrate(pattern));
+    console.log(pattern)
+})
+
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 5000
+            }
+        },
+        "color": {
+            "value": "#eee"
+        },
+        "shape": {
+            "type": "polygon",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": true,
+                "speed": 0.2,
+                "opacity_min": 0,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 2,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 2,
+                "size_min": 0,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 100,
+            "color": "#999",
+            "opacity": 0.5,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": .5,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "resize": true
+        }
+    },
+    "retina_detect": true
+});
