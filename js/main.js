42ee21142ca4fdc200b84c892a6310530e43aa95
Braeden Smith <braedensmith2@gmail.com>
2021-05-12 04:11:28 -0500

    simple v1

--- js/main.js
+++ js/main.js
@@ -1,68 +1,21 @@
-const buttons = [...document.getElementsByClassName('button')]
-buttons.forEach(elem => {
-    ['mousedown', 'touchstart'].forEach(e => elem.addEventListener(e, () => elem.classList.add('inset')));
-    ['mouseup', 'touchend', 'dragend'].forEach(e => elem.addEventListener(e, () => elem.classList.remove('inset')));
+document.addEventListener('mousemove', (e) => {
+    const g = document.getElementById('glow');
+    g.style.left = `${e.clientX}px`;
+
+    g.style.top = `${e.clientY}px`;
+});
+document.addEventListener('touchmove', (e) => {
+    const g = document.getElementById('glow');
+    g.style.left = `${e.touches[0].pageX}px`;
+
+    g.style.top = `${e.touches[0].pageY}px`;
 });
 
-const underlines = [...document.getElementsByTagName('u')]
-underlines.forEach(elem => {
-    document.addEventListener('keydown', e => {
-        if (e.key.toUpperCase() === elem.innerText)
-            elem.parentElement.click()
-    })
-});
 
-let [dx, dy] = [0, 0];
-let moveEnabled = false;
-const dpi = 1 || window.devicePixelRatio;
-['mousedown', 'touchstart'].forEach(event => {
-    document.getElementById('menubar').addEventListener(event, (e) => {
-        e.preventDefault();
 
-        let tempX, tempY
-        if (e instanceof TouchEvent) {
-            tempX = e.touches[0].pageX
-            tempY = e.touches[0].pageY
-        } else {
-            tempX = e.clientX;
-            tempY = e.clientY;
-        }
-        const mainWindow = document.getElementById('main-window')
-        dx = tempX * dpi - mainWindow.getBoundingClientRect().left || 0
-        dy = tempY * dpi - mainWindow.getBoundingClientRect().top || 0
-        moveEnabled = true;
-        mainWindow.style.cursor = 'grabbing'
-    })
-});
-
-['mousemove', 'touchmove'].forEach(event => {
-    window.addEventListener(event, (e) => {
-        if (!moveEnabled)
-            return;
-        e.preventDefault();
-
-        let tempX, tempY
-        if (e instanceof TouchEvent) {
-            tempX = e.touches[0].pageX
-            tempY = e.touches[0].pageY
-        } else {
-            tempX = e.clientX;
-            tempY = e.clientY;
-        }
-        const mainWindow = document.getElementById('main-window')
-        mainWindow.style.margin = "0";
-        mainWindow.style.left = `${tempX * dpi - dx}px`
-        mainWindow.style.top = `${tempY * dpi - dy}px`
-    }, { passive: false });
-});
-
-['mouseup', 'touchend', 'touchcancel'].forEach(event => {
-    window.addEventListener(event, (e) => {
-        moveEnabled = false;
-        const mainWindow = document.getElementById('main-window')
-        mainWindow.style.cursor = ''
-    }, { passive: false });
-});
-
-window.addEventListener('resize', () => location.reload());
-
+// document.addEventListener('mouseleave', () => {
+//     document.getElementById('glow').style.opacity = 0;
+// });
+// document.addEventListener('mouseenter', () => {
+//     document.getElementById('glow').style.opacity = 1;
+// })
