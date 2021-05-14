786de89db9b5cfbc477c900805ddf6e1a26cfb40
Braeden Smith <braedensmith2@gmail.com>
2021-05-14 05:40:56 -0500

    WIP

--- js/main.js
+++ js/main.js
@@ -47,75 +47,110 @@ document.getElementById('emoji').addEventListener('copy', () => {
     console.log(pattern)
 })
 
-particlesJS("particles-js", {
-    "particles": {
-        "number": {
-            "value": 100,
-            "density": {
-                "enable": true,
-                "value_area": 5000
-            }
-        },
-        "color": {
-            "value": "#eee"
-        },
-        "shape": {
-            "type": "polygon",
-            "stroke": {
-                "width": 0,
-                "color": "#000000"
-            },
-            "polygon": {
-                "nb_sides": 5
-            }
-        },
-        "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-                "enable": true,
-                "speed": 0.2,
-                "opacity_min": 0,
-                "sync": false
-            }
-        },
-        "size": {
-            "value": 2,
-            "random": true,
-            "anim": {
-                "enable": true,
-                "speed": 2,
-                "size_min": 0,
-                "sync": false
-            }
-        },
-        "line_linked": {
-            "enable": true,
-            "distance": 100,
-            "color": "#999",
-            "opacity": 0.5,
-            "width": 1
-        },
-        "move": {
-            "enable": true,
-            "speed": .5,
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false,
-            "attract": {
-                "enable": false,
-                "rotateX": 600,
-                "rotateY": 1200
+// particlesJS("particles-js", {
+//     "particles": {
+//         "number": {
+//             "value": 100,
+//             "density": {
+//                 "enable": true,
+//                 "value_area": 5000
+//             }
+//         },
+//         "color": {
+//             "value": "#eee"
+//         },
+//         "shape": {
+//             "type": "polygon",
+//             "stroke": {
+//                 "width": 0,
+//                 "color": "#000000"
+//             },
+//             "polygon": {
+//                 "nb_sides": 5
+//             }
+//         },
+//         "opacity": {
+//             "value": 0.5,
+//             "random": false,
+//             "anim": {
+//                 "enable": true,
+//                 "speed": 0.2,
+//                 "opacity_min": 0,
+//                 "sync": false
+//             }
+//         },
+//         "size": {
+//             "value": 2,
+//             "random": true,
+//             "anim": {
+//                 "enable": true,
+//                 "speed": 2,
+//                 "size_min": 0,
+//                 "sync": false
+//             }
+//         },
+//         "line_linked": {
+//             "enable": true,
+//             "distance": 100,
+//             "color": "#999",
+//             "opacity": 0.5,
+//             "width": 1
+//         },
+//         "move": {
+//             "enable": true,
+//             "speed": .5,
+//             "direction": "none",
+//             "random": true,
+//             "straight": false,
+//             "out_mode": "out",
+//             "bounce": false,
+//             "attract": {
+//                 "enable": false,
+//                 "rotateX": 600,
+//                 "rotateY": 1200
+//             }
+//         }
+//     },
+//     "interactivity": {
+//         "detect_on": "canvas",
+//         "events": {
+//             "resize": true
+//         }
+//     },
+//     "retina_detect": true
+// });
+
+const c = document.getElementById('canvas')
+const ctx = c.getContext('2d')
+const dpi = window.devicePixelRatio || 1;
+const scale = .1
+const interpolate = 30 // number of frames to transition over
+let frame = 0
+canvas.width = c.clientWidth * dpi * scale;
+canvas.height = c.clientHeight * dpi * scale;
+let currentFrame = new Array(canvas.width * canvas.height).fill(0).map(() => Math.random() < 0.5 ? 0 : 1)
+setInterval(() => {
+    const canvasData = ctx.createImageData(canvas.width, canvas.height)
+    const nextFrame = currentFrame.map((e, i) => {
+        for (let j = 0; j < 3; j++)
+            canvasData.data[i * 4 + j] = e ? 50 * Math.abs(frame - interpolate / 2) / interpolate : 0
+        canvasData.data[i * 4 + 3] = 50;
+
+        if (frame != interpolate/2)
+            return e;
+
+        let sum = 0
+        for (let x = -1; x <= 1; x++) {
+            for (let y = -1; y <= 1; y++) {
+                const idx = i + canvas.width * y + x
+                if (idx >= 0 && idx < currentFrame.length)
+                    sum += currentFrame[idx]
             }
         }
-    },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-            "resize": true
-        }
-    },
-    "retina_detect": true
-});
+        return sum == 3 ? 1 : sum == 4 ? e : 0
+    })
+
+    ctx.putImageData(canvasData, 0, 0);
+    currentFrame = nextFrame
+    frame = (frame + 1) % interpolate;
+}, 50)
